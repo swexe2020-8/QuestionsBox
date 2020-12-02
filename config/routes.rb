fe74e6129419cdada0008a3659e252a873f91891
okubo <1811140031g@ed.fuk.kindai.ac.jp>
2020-12-02 06:52:08 +0000

    ルーティング試作

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'top#main'
+  get 'top/main'
+  resources :questions
+  resources :replyboxes, only: [:new, :create, :destroy]
+  resources :replys, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
